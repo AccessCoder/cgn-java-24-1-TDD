644fb449d85ee7d5506a3ff2022a4e8b7ca4b15b
Dominic <andro5@web.de>
2024-03-11 10:55:59 +0100

    Initial commit

---
+++ src/main/java/de/neuefische/FizzBuzz.java
@@ -0,0 +1,16 @@
+package de.neuefische;
+
+public class FizzBuzz {
+
+
+    public static String play(int input) {
+        if (input%3==0 && input%5==0){
+            return "FIZZBUZZ";
+        } else if (input%5==0) {
+            return "BUZZ";
+        } else if (input%3==0) {
+            return "FIZZ";
+        }
+        return ""+input;
+    }
+}
