55f79f5def06b8e1aef0b76d0897a1cac564bf4b
Dominic <andro5@web.de>
2024-03-12 10:05:01 +0100

    Add maven-surefire-plugin

--- src/test/java/de/neuefische/PlayerCharacterTest.java
+++ src/test/java/de/neuefische/PlayerCharacterTest.java
@@ -1,11 +1,8 @@
 package de.neuefische;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerCharacterTest {
 
